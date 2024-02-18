788db16bf58aaf27e6b9d25b3a675fce582420d0
Alexey Sidorov <alexey.sidorov@easybrain.com>
2024-02-18 19:53:29 +0300

    move files in different folders

--- Assets/Scripts/GamePlayController.cs
+++ Assets/Scripts/GamePlayController.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class GamePlayController : MonoBehaviour
 {
     [SerializeField] private GameObject _point;
+    [SerializeField] private Button _fieldButton;
     private float _playerSpeed = 3f;
     private float _playerRotationSpeed = 3f;
     
+    
     void Start()
     {
-        // _fieldButton.onClick.AddListener(MoveToPoint);
+        _fieldButton.onClick.AddListener(MoveToPoint);
     }
 
     void FixedUpdate()
