54c23342c0571ab9a41d51b0730492ae401f8f12
Alexey Sidorov <alexey.sidorov@easybrain.com>
2024-04-04 08:31:49 +0300

    add new logic starship game

---
+++ Assets/Scripts/RocketShipController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RocketShipController : MonoBehaviour
+{
+    [SerializeField] private WayPointSpawner _currentWayPoint;
+    [SerializeField] private float _shipSpeed;
+
+    void Update()
+    {
+        if (_currentWayPoint.Target() == null)
+        {
+            return;
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, _currentWayPoint.Target().position, _shipSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Mathf.Atan2(_currentWayPoint.Target().position.y - transform.position.y, _currentWayPoint.Target().position.x - transform.position.x) * Mathf.Rad2Deg - 90);
+        
+    }
+}
