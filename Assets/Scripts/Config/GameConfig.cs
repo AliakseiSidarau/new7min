b6bcc99eaa85872e10e3140a167531ef251e7b55
Alexey <39629823+AliakseiSidarau@users.noreply.github.com>
2024-04-10 10:36:33 +0300

    small changes for game config file

--- Assets/Scripts/Config/GameConfig.cs
+++ Assets/Scripts/Config/GameConfig.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 [CreateAssetMenu (fileName = "Config", menuName = "GameData/Config", order = 1)] 
 public class GameConfig : ScriptableObject
 {
-    public float playerSpeed;
-    public float playerRotationSpeed;
-    public Vector3 startPoint;
+    public float shipSpeed;
+    public int diamondScore;
+    public float meteorSpeed;
+    public float meteorRotationSpeed;
 }
