faf7d25b99f3901c5ba561d0a6f602e330154749
BeardedMan <myworkprod@gmail.com>
2024-06-19 23:35:06 +0300

    diamon spawner scrip draft version

--- Assets/Scripts/Scenes/GamePlay/DiamondSpawner.cs
+++ Assets/Scripts/Scenes/GamePlay/DiamondSpawner.cs
@@ -1,35 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DiamondSpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject _diamond;
+    private int _count;
+    //создать публичный метод который будет отнимать от каунтера 1
 
-    // the range of X
-    [Header("X Spawn Range")]
-    public float xMin;
-    public float xMax;
+    void Start() {
 
-    // the range of y
-    [Header("Y Spawn Range")]
-    public float yMin;
-    public float yMax;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Defines the min and max ranges for x and y
-        Vector2 pos = new Vector2(Random.Range(xMin, xMax), Random.Range(yMin, yMax));
-
-        _diamond.transform.position = pos;
+        _count = 0;
+            
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+         if (_count<1)
+         {
+            _count++;
+
+            GameObject newObj = Instantiate(_diamond, new Vector3(RandomSpawnPosition(), RandomSpawnPosition()), Quaternion.identity);
+            newObj.transform.position = new Vector3(RandomSpawnPosition(), RandomSpawnPosition(), 0);
+            Debug.Log("Diamond created");
+         }
+
+    }
+
+    private float RandomSpawnPosition()
+    {
+
+        return Random.Range(-5f, 5f);
+
     }
 
 }
